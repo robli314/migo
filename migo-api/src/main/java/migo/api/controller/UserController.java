7266cbaec5855f41b4ca63aa05988b5b0b889bf1
Pires-at-344153881143 <orobsonpires@192.168.0.102>
2019-01-23 01:06:15 +0100

    #1 Initial commit

---
+++ migo-api/src/main/java/migo/api/controller/UserController.java
@@ -0,0 +1,19 @@
+package migo.api.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+
+	public UserController() {
+
+	}
+	
+	@RequestMapping("/hello")
+    public String greeting(@RequestParam(value="name", defaultValue="World") String name) {
+        return "Hi, there!";
+    }
+
+}
